67d231dd059e67df9e0ca5c0002d0c5e3a3248a1
Eduardo Henrique <itsduzao@outlook.com>
2026-02-08 21:59:53 -0300

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/logikee-cep-lookup/', // ✅ Nome do seu repositório GitHub
   plugins: [
     tailwindcss(),
     tsconfigPaths(),
